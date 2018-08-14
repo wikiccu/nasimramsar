41f38785b51eb004b693e8253dd26b71f3baaa86
mohsen shabanian <mohsen1299@gmail.com>
2018-08-14 14:50:58 +0430

    make post migrate

--- database/migrations/2018_08_14_095557_create_posts_table.php
+++ database/migrations/2018_08_14_095557_create_posts_table.php
@@ -15,6 +15,19 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('pic',400);
+            $table->string('title',400)->collation('utf8_unicode_ci');
+            $table->string('abstract',1000)->collation('utf8_unicode_ci');
+            $table->text('body')->collation('utf8_unicode_ci');
+
+            $table->dateTime('createdtime');
+            $table->dateTime('editedtime');
+
+            $table->tinyInteger('published')->default(0);
+
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
