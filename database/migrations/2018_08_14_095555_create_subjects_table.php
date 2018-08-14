9f9bf367091ea3db6dd710de60ee4369aaa04c96
mohsen shabanian <mohsen1299@gmail.com>
2018-08-14 19:19:00 +0430

    make subject table

--- database/migrations/2018_08_14_095555_create_subjects_table.php
+++ database/migrations/2018_08_14_095555_create_subjects_table.php
@@ -15,6 +15,8 @@ class CreateSubjectsTable extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title', 400);
+            $table->integer('order')->unsigned()->default(0);
             $table->timestamps();
         });
     }
