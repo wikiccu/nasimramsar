9a2942bcee9998d5a9ecf2214d0d12943b7ba4c3
mohsen shabanian <mohsen1299@gmail.com>
2018-08-16 19:30:34 +0430

    add relation user2role

--- database/migrations/2018_08_14_095945_create_teacher_course_table.php
+++ database/migrations/2018_08_14_095945_create_teacher_course_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeacherscoursesTable extends Migration
+class CreateTeacherCourseTable extends Migration
 {
     /**
      * Run the migrations.
