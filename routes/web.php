19833b78e8944551d379754cfc1ab32b7258b926
mohsen shabanian <mohsen1299@gmail.com>
2018-10-22 12:55:18 +0330

    add sitemap

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,61 @@ Route::DELETE('/fileuploads', 'Admin\AdminController@destroy');
 //image in gallery
 Route::DELETE('/deleteGalleryImage', 'Admin\GalleryController@destroyImage');
 Route::post('/changeGalleryImageTitle', 'Admin\GalleryController@changeImageTitle');
+
+//site-map
+//https://gitlab.com/Laravelium/Sitemap
+Route::get('sitemap', function() {
+
+	// create new sitemap object
+	$sitemap = App::make('sitemap');
+
+	// set cache key (string), duration in minutes (Carbon|Datetime|int), turn on/off (boolean)
+	// by default cache is disabled
+	$sitemap->setCache('laravel.sitemap', 60);
+
+	// check if there is cached sitemap and build new only if is not
+	if (!$sitemap->isCached()) {
+		// add item to the sitemap (url, date, priority, freq)
+		$sitemap->add(URL::to(''), '2018-08-25T20:10:00+02:00', '1.0', 'daily');
+		$sitemap->add(URL::to('news'), '2018-08-26T12:30:00+02:00', '0.9', 'daily');
+		$sitemap->add(URL::to('gallery'), '2018-08-26T12:30:00+02:00', '0.9', 'daily');
+		$sitemap->add(URL::to('about'), '2018-08-26T12:30:00+02:00', '0.9', 'monthly');
+		$sitemap->add(URL::to('contact'), '2018-08-26T12:30:00+02:00', '0.9', 'monthly');
+
+		// get all posts from db
+		$posts = DB::table('posts')->orderBy('created_at', 'desc')->get();
+
+		// add every post to the sitemap
+		foreach ($posts as $post) {
+			$sitemap->add('/post/'.$post->id, $post->updated_at, $post->published,'daily');
+        }
+
+		// get all courses from db
+		$courses = DB::table('courses')->orderBy('created_at', 'desc')->get();
+
+		// add every course to the sitemap
+		foreach ($courses as $course) {
+			$sitemap->add('/course/'.$course->id, $course->updated_at, '0.5','daily');
+        }
+
+		// get all teachers from db
+		$teachers = DB::table('teachers')->orderBy('created_at', 'desc')->get();
+
+		// add every teacher to the sitemap
+		foreach ($teachers as $teacher) {
+			$sitemap->add('/teacher/'.$teacher->id, $teacher->updated_at, '0.5','daily');
+        }
+
+
+		// get all teachers from db
+		$galleryImages = DB::table('images')->orderBy('created_at', 'desc')->get();
+
+		// add every teacher to the sitemap
+		foreach ($galleryImages as $galleryImage) {
+			$sitemap->add('/galleryImage/'.$galleryImage->id, $galleryImage->updated_at, '0.5','daily');
+		}
+	}
+
+	// show your sitemap (options: 'xml' (default), 'html', 'txt', 'ror-rss', 'ror-rdf')
+	return $sitemap->render('xml');
+});
